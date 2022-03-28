66e4a15a58366264d8b4def9b71fee5929b5b13e
ocampojosue <ocampojosue08@gmail.com>
2022-03-28 19:34:31 -0400

    Paquetes instalados laravel ui, livewire

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        // User::factory(1)->create();ejecutar factory
+        // $this->call(UserSeeder::class); ejecutar seeder
+        $this->call(UserSeeder::class);
+    }
+}
